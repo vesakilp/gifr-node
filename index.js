f2b66a534880fe7a9f20cb252f4c835b9baa369d
Vesa Kilpiäinen <vesakilp@google.com>
2016-04-01 21:30:43 +0300

    removed gifr app

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ var express = require('express'),
     redis = require('redis');
 
 var app = express();
-var gifr = express();
 
 console.log(process.env.REDIS_PORT_6379_TCP_ADDR + ':' + process.env.REDIS_PORT_6379_TCP_PORT);
 
@@ -16,19 +15,12 @@ console.log(process.env.REDIS_PORT_6379_TCP_ADDR + ':' + process.env.REDIS_PORT_
 // APPROACH 2: Using host entries created by Docker in /etc/hosts (RECOMMENDED)
 var client = redis.createClient('6379', 'redis');
 
-
 app.get('/', function(req, res, next) {
   client.incr('counter', function(err, counter) {
     if(err) return next(err);
     res.send('This page has been viewed ' + counter + ' times! HAHADDD');
   });
 });
-
-gifr.get('/', function (req, res) {
-  console.log('gifr request' req);
-});
-
-app.use('/gifr', gifr); //token url here?
 
 
 http.createServer(app).listen(process.env.PORT || 8080, function() {
